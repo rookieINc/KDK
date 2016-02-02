0cc5a691f2fff8b81cdccfbb568c81f0e48ba934
Kazama <LateToad@gmail.com>
2016-02-02 21:10:36 +0800

    v0.9.0

---
+++ src/kdk_log.c
@@ -0,0 +1,16 @@
+
+/************************************************************
+ * Copyright(C), 2015-2016, QTPAY
+ * 
+ * FileName    : kdk_log.c
+ * Author      : rookieINc
+ * Version     : v0.1.0
+ * Date        : 2016-02-01 14:05
+ * Description : kdk_log.c
+ * History     : 
+ ***********************************************************/
+
+
+#include "kdk_log.h"
+
+
---
+++ src/kdk_log.h
@@ -0,0 +1,29 @@
+
+/************************************************************
+ * Copyright(C), 2015-2016, QTPAY
+ * 
+ * FileName    : kdk_log.h
+ * Author      : rookieINc
+ * Version     : v0.1.0
+ * Date        : 2016-02-01 14:05
+ * Description : kdk_log.h
+ * History     : 
+ ***********************************************************/
+
+
+#ifndef _KDK_LOG_H_INCLUDED_
+#define _KDK_LOG_H_INCLUDED_
+
+#include "kdk_base.h"
+
+kdk_uint32
+kdk_log_init();
+
+kdk_uint32
+kdk_log_write();
+
+kdk_void
+kdk_log_destroy();
+
+
+#endif /* _KDK_LOG_H_INCLUDED_ */
